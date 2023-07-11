c2d293e7c923c01c0e2b4fd79ccfca7700f5bec7
pedro0058 <pedro2015765@gmail.com>
2023-07-10 22:03:04 -0300

    Update ProdutorSimples.java

--- testekafka/src/main/java/testekafka/ProdutorSimples.java
+++ testekafka/src/main/java/testekafka/ProdutorSimples.java
@@ -12,7 +12,9 @@ import org.apache.kafka.common.serialization.StringSerializer;
 public class ProdutorSimples {
 	
 	private static Random random = new Random();
-	public static Sensor termometro = new Sensor("Termometro", random.nextDouble(24,32));
+	public static Double inicialTemp = random.nextDouble(24,32);
+	public static Sensor termometro = new Sensor("Termometro", inicialTemp);
+	public static Double somaTemperatura = inicialTemp;
 
 	public static void main(String[] args) throws InterruptedException {
 		// Configuração das propriedades do produtor
@@ -24,7 +26,16 @@ public class ProdutorSimples {
         try(Producer <String,Sensor> producer = new KafkaProducer <>(props)){
         while(true) {
 			// Atualiza o valor do termômetro com um novo valor aleatório entre 24 e 32
-        	termometro.setValor(random.nextDouble(24,32));
+        	Double temperatura = random.nextDouble(24,32);
+        	somaTemperatura = somaTemperatura + temperatura;
+        	termometro.setValor(temperatura);
+        	termometro.setMedia(somaTemperatura/termometro.getLeituras());
+        	if(temperatura > termometro.getMaxValor()) {
+        		termometro.setMaxValor(temperatura);
+        	}
+        	if(temperatura < termometro.getMinValor()) {
+        		termometro.setMinValor(temperatura);
+        	}
 			// Cria um novo registro de produtor com o tópico "testejava" e o objeto Sensor
         	ProducerRecord <String,Sensor> record = new ProducerRecord<>("testejava", termometro);
 			// Envia o registro para o Kafka
